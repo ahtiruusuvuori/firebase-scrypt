51c13515c40e748a51ff86955f1f5718d6c333ef
Henri Kankaanpää <henri.kankaanpaa@geniem.com>
2020-02-18 21:18:58 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
-echo '  [x] Install SLL dev dependency'
-sudo apt install libssl-dev 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        sudo apt install libssl-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install openssl
+        export PATH="/usr/local/opt/openssl/bin:$PATH"
+        export LDFLAGS="-L/usr/local/opt/openssl/lib"
+        export CPPFLAGS="-I/usr/local/opt/openssl/include"
+fi
 
-echo '  [x] Install automake'
-sudo apt install automake
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	sudo apt install automake
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install automake
+fi
 
-echo '  [x] Clone firebase Scrypt repo'
-git clone https://github.com/firebase/scrypt.git
-
-echo '  [x] Build Scrypt'
 cd scrypt/
 autoreconf -i
 ./configure
