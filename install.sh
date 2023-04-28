aaf730bc2e526a63758393ca635a9a7ce8e52240
Ahti Ruusuvuori <ahti.ruusuvuori@geniem.com>
2023-04-28 14:47:03 +0300

    fixed install bash script syntax

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-if [ "$OSTYPE" == "linux-gnu" ] || [ "$OSTYPE" == "linux-musl" ]; then
+if [ "$OSTYPE" = "linux-gnu" ] || [ "$OSTYPE" = "linux-musl" ]; then
         apk add --no-chache libssl-dev
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" = "darwin"* ]]; then
         brew install openssl
         export PATH="/usr/local/opt/openssl/bin:$PATH"
         export LDFLAGS="-L/usr/local/opt/openssl/lib"
         export CPPFLAGS="-I/usr/local/opt/openssl/include"
 fi
 
-if [ "$OSTYPE" == "linux-gnu" ] || [ "$OSTYPE" == "linux-musl" ]; then
+if [ "$OSTYPE" = "linux-gnu" ] || [ "$OSTYPE" = "linux-musl" ]; then
 	apk add --no-chache automake
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" = "darwin"* ]]; then
 	brew install automake
 fi
 
